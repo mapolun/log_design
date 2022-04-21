a7ee85f415e8eee8816a8bea19caf4497c44a984
wtt <1364576479@qq.com>
2022-04-21 13:29:15 +0800

    add:注释

--- examples.php
+++ examples.php
@@ -13,16 +13,17 @@ use LoggerDesign\Logger;
 use LoggerDesign\Proxy\LoggerProxy;
 
 $logger = getLogger();
-$logger->info("123");
+$logger->info("测试");
 
 function getLogger() : LoggerProxy
 {
     try{
 
+        //代理注入logger类
         $logger = new LoggerProxy(Logger::class);
 
+        //设置logger后置服务，切面
         $convertors = getAfterGroup();
-
         $logger->setConvertors($convertors);
 
         return $logger;
@@ -37,7 +38,9 @@ function getLogger() : LoggerProxy
 function getAfterGroup() : array
 {
     $containers = new Containers();
+    //加载textService
     $containers->add("text",TextService::class);
+    //价值meilisearchService
     $containers->add("meilisearch",MeiliSearchService::class);
     return $containers->get();
 }
